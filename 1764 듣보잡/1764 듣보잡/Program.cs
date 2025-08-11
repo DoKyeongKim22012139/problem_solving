097e9e200ca760077c536e7f7464424b24ae9d6e
DoKyeong Kim <kjkkjs5252@gmail.com>
2025-08-11 20:04:47 +0900

    백준 1764
    문제를 잘봐라
    trim은 공백을 제거해줌 굳이 필요없을지도 이번에는 숫자출력을 안봐서 자꾸 틀리는거였음

---
+++ 1764 듣보잡/1764 듣보잡/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace _1764_듣보잡
+{
+    internal class Program
+    {
+        static HashSet<string> names = new HashSet<string>();
+        static List<string> answer = new List<string>();
+        static void Main(string[] args)
+        {
+            solve();
+        }
+
+        static public void solve()
+        {
+            var reader = new StreamReader(Console.OpenStandardInput());
+            var writer = new StreamWriter(Console.OpenStandardOutput());
+            string[] input = reader.ReadLine().Split(" ");
+            int n = int.Parse(input[0]);
+            int m = int.Parse(input[1]);
+
+            int sum = n + m;
+
+            for(int i=0; i<n;i++)
+            {
+                string name = reader.ReadLine().Trim();
+                names.Add(name);
+            }
+
+            for(int i=0;i<m;i++)
+            {
+               string name = reader.ReadLine().Trim() ;
+                if(names.Contains(name))
+                {
+                    answer.Add(name);
+                }
+            }
+
+            answer.Sort();
+
+            writer.WriteLine(answer.Count);
+            for(int i=0;i<answer.Count;i++)
+            {
+                writer.WriteLine(answer[i]);
+            }
+            writer.Flush(); 
+        }
+    }
+
+}
