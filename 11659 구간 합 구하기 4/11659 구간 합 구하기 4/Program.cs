ed15f3cb016b7f28afc7a04dfde1a3109f72b6b4
DoKyeong Kim <kjkkjs5252@gmail.com>
2025-08-12 20:35:17 +0900

    백준 11659
    이건 하나씩 더하는것보다 누적합을 이용하는게 더빠름

---
+++ 11659 구간 합 구하기 4/11659 구간 합 구하기 4/Program.cs
@@ -0,0 +1,29 @@
+﻿
+var reader = new StreamReader(Console.OpenStandardInput());
+var writer = new StreamWriter(Console.OpenStandardOutput());
+
+string[] input = reader.ReadLine().Split(" ");
+
+int N= int.Parse(input[0]);
+int M= int.Parse(input[1]);
+
+int[] answer = new int[N + 1];
+answer[0] = 0;
+string[] nums = reader.ReadLine().Split(" ");
+for (int i = 1; i <=N; i++)
+{
+    answer[i] =answer[i - 1]+int.Parse(nums[i-1]);
+}
+
+for(int i=0;i<M;i++)
+{
+    string[] range = reader.ReadLine().Split(" ");
+    int start = int.Parse(range[0]);
+    int last= int.Parse(range[1]);
+
+    int sum = answer[last]-answer[start-1];
+    writer.WriteLine(sum);
+}
+
+
+writer.Flush();
